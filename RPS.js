b81f50d598eaae98858967ad1c28d7e1846ba12c
John Anderson <djanyreason@gmail.com>
2023-04-18 10:35:18 -0400

    Adds playRound function to evaluate an RPS round
    playRound function takes two parameters: playerChoice and
    computerChoice. These must each be strings that are "rock", "paper"
    or "scissors" (case is irrelevant). Function validates inputs are
    correct, then determines the outcome of a round of Rock, Paper,
    Scissors and returns a string telling the player the outcome.

--- RPS.js
+++ RPS.js
@@ -1,8 +1,38 @@
-// Function - takes no parameters; returns a string of "Rock", "Paper", or "Scissors" with 1/3rd probability of each
+// Function getComputerChoice() - Randomly selects a choice of Rock, Paper, or Scissors
+// Takes no parameters
+// Returns a string of "Rock", "Paper", or "Scissors"
 function getComputerChoice() {
     const choice = Math.random() * 3;
 
     if(choice < 1) return "Rock";
     else if (choice < 2) return "Paper";
     else return "Scissors";
+}
+
+// Function playRound(playerChoice, computerChoice) - Evaluates a round of RPS
+// Parameter playerChoice and computerChoice are strings
+//      must be "rock", "paper", or "scissors", capitalization irrelevant
+// Returns a String; either "You [Win/Lose]! X beats Y" or "Draw! Both choose Z"
+function playRound(playerChoice, computerChoice) {
+    // Create local variables for choices with standardized capitalization
+    const pCStd = playerChoice.substr(0,1).toUpperCase() + playerChoice.substr(1,playerChoice.length - 1).toLowerCase();
+    const cCStd = computerChoice.substr(0,1).toUpperCase() + computerChoice.substr(1,computerChoice.length - 1).toLowerCase();
+
+    //Verify that both inputs are valid
+    if(!(pCStd === "Rock" || pCStd === "Paper" || pCStd === "Scissors"))
+        return "Invalid Player Selection";
+    else if (!(cCStd === "Rock" || cCStd === "Paper" || cCStd === "Scissors"))
+        return "Invalid Computer Selection";
+    
+    //Checks for a Draw
+    if(pCStd === cCStd) return "Draw! Both choose " + pCStd;
+
+    //Checks if Player wins
+    if((pCStd === "Rock" && cCStd === "Scissors") ||
+       (pCStd === "Paper" && cCStd === "Rock") ||
+       (pCStd === "Scissors" && cCStd === "Paper"))
+        return "You Win! " + pCStd + " beats " + cCStd;
+    
+    //If inputs are valid, not a Draw, not Player win, then Computer must win
+    else return "You Lose! " + cCStd + " beats " + pCStd;
 }
