bd7be4bebb328b6d93ce7b431ed61cdaca3838d4
John Anderson <djanyreason@gmail.com>
2023-04-18 11:36:37 -0400

    Adds game() function to play RPS in console
    The game() function plays a best-of-5 Rock Paper Scissors game in the
    console, using console.log to display game state, round results, and
    final results, and using prompt() to get player input.

--- RPS.js
+++ RPS.js
@@ -36,3 +36,55 @@ function playRound(playerChoice, computerChoice) {
     //If inputs are valid, not a Draw, not Player win, then Computer must win
     else return "You Lose! " + cCStd + " beats " + pCStd;
 }
+
+// Function game() - Plays a best-of-5 game of Rock Paper Scissors in the Console
+//      Prints game status by round and final result
+// No Parameters
+// Does not return
+function game() {
+    // Variables to track game state
+    var round = 0;
+    var playerScore = 0;
+    var computerScore = 0;
+
+    // Variables for each round's results
+    var playerChoice;
+    var computerChoice;
+    var roundResult;
+
+    while(round < 5) { // Main loop to run best-of-5 
+
+        // Increment round number and print current game state
+        round++;
+        console.log('Round ' + round + ': Score Player ' + playerScore + ' - Computer ' + computerScore);
+
+        // Nesting While Loops until Player or Computer wins the round
+        // Inner loop runs until player gives valid input, and exits with roundResult showing the result
+        // Outer loop runs until the result is not a draw
+        
+        roundResult = "Draw"; //initialize for outer loop
+
+        while(roundResult.substring(0,4) === "Draw") {
+            computerChoice = getComputerChoice();
+            roundResult = "Invalid"; //initialize for inner loop
+            while(roundResult.substring(0,7) === "Invalid") {
+                playerChoice = prompt("Enter your selection for this round:");
+                roundResult = playRound(playerChoice, computerChoice);
+                console.log(roundResult);
+            }
+        }
+
+        // Check if Player won the round, and update score appropriately
+        if(roundResult.substring(4,5) === "W") playerScore++;
+        else if (roundResult.substring(4,5) === "L") computerScore++;
+        
+        // Check if either Player or Computer has won 3 games (3 wins is Best-of-5)
+        if(Math.max(playerScore, computerScore) === 3) break;
+    }
+
+    // Print final outcome
+    console.log("Game Over!");
+    console.log("Final Score: Player " + playerScore + " - Computer " + computerScore);
+    if(playerScore > computerScore) console.log ("Player Wins!");
+    else console.log("Computer Wins!");
+}
